2f39353a0a7c8e9133e33321a05a0708e48052dd
MUNEZERO Aline <166438999+munezeromas@users.noreply.github.com>
2025-05-25 06:33:27 +0200

    Working on this class...........

--- src/main/java/org/example/demoapp/User/InMemoryUserRepository.java
+++ src/main/java/org/example/demoapp/User/InMemoryUserRepository.java
@@ -8,34 +8,5 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
-    private final Map<Long, User> users = new ConcurrentHashMap<>();
-    private long idCounter = 1;
-
-    @Override
-    public User save(User user) {
-        user.setId(idCounter++);
-        users.put(user.getId(), user);
-        return user;
-    }
-
-    @Override
-    public Optional<User> findById(Long id) {
-        return Optional.ofNullable(users.get(id));
-    }
-
-    @Override
-    public List<User> findAll() {
-        return new ArrayList<>(users.values());
-    }
-
-    @Override
-    public User update(User user) {
-        users.put(user.getId(), user);
-        return user;
-    }
-
-    @Override
-    public void delete(Long id) {
-        users.remove(id);
-    }
+  
 }
